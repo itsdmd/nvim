0472b19d988bcf862d95f088505d76a75ce56096
Đức Đào <duc.dao.431@gmail.com>
2023-12-11 10:51:34 +0700

    formatting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,121 +3,126 @@
 -- Add any additional keymaps here
 -- ----------- Navigation ----------- --
 vim.api.nvim_set_keymap("n", "n", "h", {
-  noremap = true,
+	noremap = true,
 })
 vim.api.nvim_set_keymap("n", "e", "j", {
-  noremap = true,
+	noremap = true,
 })
 vim.api.nvim_set_keymap("n", "i", "k", {
-  noremap = true,
+	noremap = true,
 })
 vim.api.nvim_set_keymap("n", "o", "l", {
-  noremap = true,
+	noremap = true,
 })
 
 vim.api.nvim_set_keymap("v", "n", "h", {
-  noremap = true,
+	noremap = true,
 })
 vim.api.nvim_set_keymap("v", "e", "j", {
-  noremap = true,
+	noremap = true,
 })
 vim.api.nvim_set_keymap("v", "i", "k", {
-  noremap = true,
+	noremap = true,
 })
 vim.api.nvim_set_keymap("v", "o", "l", {
-  noremap = true,
+	noremap = true,
 })
 
 vim.api.nvim_set_keymap("n", "<S-h>", "<S-n>", {
-  noremap = true,
+	noremap = true,
 })
 vim.api.nvim_set_keymap("n", "<S-l>", "<S-o>", {
-  noremap = true,
+	noremap = true,
 })
 vim.api.nvim_set_keymap("v", "<S-h>", "<S-n>", {
-  noremap = true,
+	noremap = true,
 })
 vim.api.nvim_set_keymap("v", "<S-l>", "<S-o>", {
-  noremap = true,
+	noremap = true,
 })
 
 -- Next search result
 vim.api.nvim_set_keymap("n", "j", "n", {
-  noremap = true,
+	noremap = true,
 })
 
 -- Window navigation
 vim.api.nvim_set_keymap("n", "<C-n>", "<C-h>", {
-  noremap = false,
+	noremap = false,
 })
 vim.api.nvim_set_keymap("n", "<C-e>", "<C-j>", {
-  noremap = false,
+	noremap = false,
 })
 vim.api.nvim_set_keymap("n", "<C-i>", "<C-k>", {
-  noremap = false,
+	noremap = false,
 })
 vim.api.nvim_set_keymap("n", "<C-o>", "<C-l>", {
-  noremap = false,
+	noremap = false,
+})
+
+-- Page up/down
+vim.api.nvim_set_keymap("n", "<PageUp>", "<PageUp>zz", {
+	noremap = true,
+})
+vim.api.nvim_set_keymap("n", "<PageDown>", "<PageDown>zz", {
+	noremap = true,
 })
 
 -- Hop
 local hop = require("hop")
 -- local directions = require('hop.hint').HintDirection
 vim.keymap.set("", "f", function()
-  hop.hint_char1({
-    current_line_only = false,
-  })
+	hop.hint_char1({
+		current_line_only = false,
+	})
 end, {
-  remap = true,
+	remap = true,
+	silent = true,
 })
-vim.keymap.set("", "F", function()
-  hop.hint_char2({
-    current_line_only = false,
-  })
-end, {
-  remap = true,
-})
+vim.keymap.set("", "F", "f", { noremap = true })
 vim.keymap.set("", "t", function()
-  hop.hint_char1({
-    current_line_only = false,
-    hint_offset = -1,
-  })
+	hop.hint_char1({
+		current_line_only = false,
+		hint_offset = -1,
+	})
 end, {
-  remap = true,
+	remap = true,
+	silent = true,
 })
-vim.keymap.set("", "T", function()
-  hop.hint_char2({
-    current_line_only = false,
-    hint_offset = -1,
-  })
-end, {
-  remap = true,
-})
+vim.keymap.set("", "T", "t", { noremap = true })
 
 -- Jump to context (nvim-treesitter-context)
 vim.keymap.set("n", "[c", function()
-  require("treesitter-context").go_to_context()
+	require("treesitter-context").go_to_context()
 end, { silent = true, noremap = true })
+
+-- Telescope Project
+vim.api.nvim_set_keymap(
+	"n",
+	"<C-p>",
+	":lua require'telescope'.extensions.project.project{}<CR>",
+	{ noremap = true, silent = true }
+)
 
 -- -------------- Mode -------------- --
 vim.api.nvim_set_keymap("n", "<S-a>", "i", {
-  noremap = true,
+	noremap = true,
 })
 
 -- -------------- Line -------------- --
 -- Add a new line below
 vim.api.nvim_set_keymap("n", "l", "o", {
-  noremap = true,
+	noremap = true,
 })
 -- Add a new line above
 vim.api.nvim_set_keymap("n", "L", "O", {
-  noremap = true,
+	noremap = true,
 })
 -- Move line down with Alt + E
 vim.api.nvim_set_keymap("n", "<A-e>", "<A-j>", {
-  noremap = false,
+	noremap = false,
 })
 -- Move line up with Alt + I
 vim.api.nvim_set_keymap("n", "<A-i>", "<A-k>", {
-  noremap = false,
+	noremap = false,
 })
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -8,86 +8,100 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-  spec = { -- add LazyVim and import its plugins
-    {
-      "LazyVim/LazyVim",
-      import = "lazyvim.plugins",
-    },
-    { "smoka7/hop.nvim" },
-    { "github/copilot.vim", lazy = false },
-    --{ import = "lazyvim.plugins.extras.formatting.prettier" },
-    {
-      "stevearc/conform.nvim",
-      opts = {
-        formatters_by_ft = {
-          ["css"] = { "prettier" },
-          ["graphql"] = { "prettier" },
-          ["handlebars"] = { "prettier" },
-          ["html"] = { "prettier" },
-          ["javascript"] = { "prettier" },
-          ["javascriptreact"] = { "prettier" },
-          ["jsonc"] = { "prettier" },
-          ["json"] = { "prettier" },
-          ["less"] = { "prettier" },
-          ["markdown.mdx"] = { "prettier" },
-          ["markdown"] = { "prettier" },
-          ["scss"] = { "prettier" },
-          ["typescript"] = { "prettier" },
-          ["typescriptreact"] = { "prettier" },
-          ["vue"] = { "prettier" },
-          ["yaml"] = { "prettier" },
-        },
-      },
-    },
-    {
-      "iamcco/markdown-preview.nvim",
-      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-      ft = { "markdown" },
-      build = function()
-        vim.fn["mkdp#util#install"]()
-      end,
-    },
-    {
-      "jparise/vim-graphql",
-    },
-    {
-      "tpope/vim-surround",
-    },
-    {
-      "tomtom/tcomment_vim",
-    },
-    { "nvim-treesitter/nvim-treesitter-context" },
-    {
-      import = "plugins",
-    },
-  },
-  defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
-  },
-  install = {
-    colorscheme = { "tokyonight", "habamax" },
-  },
-  checker = {
-    enabled = true,
-  }, -- automatically check for plugin updates
-  performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip", -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
+	spec = { -- add LazyVim and import its plugins
+		{
+			"LazyVim/LazyVim",
+			import = "lazyvim.plugins",
+		},
+		{ "smoka7/hop.nvim" },
+		{ "github/copilot.vim", lazy = false },
+		--{ import = "lazyvim.plugins.extras.formatting.prettier" },
+		{
+			"stevearc/conform.nvim",
+			opts = {
+				formatters_by_ft = {
+					["css"] = { "prettier" },
+					["graphql"] = { "prettier" },
+					["handlebars"] = { "prettier" },
+					["html"] = { "prettier" },
+					["javascript"] = { "prettier" },
+					["javascriptreact"] = { "prettier" },
+					["jsonc"] = { "prettier" },
+					["json"] = { "prettier" },
+					["less"] = { "prettier" },
+					["markdown.mdx"] = { "prettier" },
+					["markdown"] = { "prettier" },
+					["scss"] = { "prettier" },
+					["typescript"] = { "prettier" },
+					["typescriptreact"] = { "prettier" },
+					["vue"] = { "prettier" },
+					["yaml"] = { "prettier" },
+				},
+			},
+		},
+		{
+			"iamcco/markdown-preview.nvim",
+			cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+			ft = { "markdown" },
+			build = function()
+				vim.fn["mkdp#util#install"]()
+			end,
+		},
+		{ "jparise/vim-graphql" },
+		{ "tpope/vim-surround" },
+		{ "tomtom/tcomment_vim" },
+		{ "mg979/vim-visual-multi" },
+		{ "nvim-treesitter/nvim-treesitter-context" },
+		{
+			"nvim-telescope/telescope-project.nvim",
+			opt = {
+				display_type = "full",
+			},
+		},
+		{ "ThePrimeagen/vim-be-good" },
+		{
+			import = "plugins",
+		},
+	},
+	defaults = {
+		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+		-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+		lazy = false,
+		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+		-- have outdated releases, which may break your Neovim install.
+		version = false, -- always use the latest git commit
+		-- version = "*", -- try installing the latest stable version for plugins that support semver
+	},
+	install = {
+		colorscheme = { "tokyonight", "habamax" },
+	},
+	checker = {
+		enabled = true,
+	}, -- automatically check for plugin updates
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip", -- "matchit",
+				-- "matchparen",
+				-- "netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+})
+
+require("telescope").setup({
+	extensions = {
+		project = {
+			base_dirs = {
+				{ "/home/itsdmd/Documents/GitHub", max_depth = 1 },
+			},
+			hidden_files = true,
+			order_by = "asc",
+		},
+	},
 })
