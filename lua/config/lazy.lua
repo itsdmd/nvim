0a977111e1a865fc6a26bac39b8986cb949a6b17
Đức Đào <duc.dao.431@gmail.com>
2023-12-13 21:23:28 +0700

    add markmap

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -75,6 +75,18 @@ require("lazy").setup({
 				vim.fn["mkdp#util#install"]()
 			end,
 		},
+		{
+			"Zeioth/markmap.nvim",
+			cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
+			opts = {
+				html_output = "/tmp/markmap.html", -- (default) Setting a empty string "" here means: [Current buffer path].html
+				hide_toolbar = false, -- (default)
+				grace_period = 0, -- (default) Stops markmap watch after 60 minutes. Set it to 0 to disable the grace_period.
+			},
+			config = function(_, opts)
+				require("markmap").setup(opts)
+			end,
+		},
 		{ "jparise/vim-graphql" },
 		{ "tpope/vim-surround" },
 		{ "tomtom/tcomment_vim" },
