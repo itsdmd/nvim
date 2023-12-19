6c81df20d781dfa3614936e1476d53b9c0cc6e46
Đức Đào <duc.dao.431@gmail.com>
2023-12-19 11:14:20 +0700

    copilot config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,20 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Enable Copilot when VimEnter
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = { "*" },
-	command = "Copilot enable",
-})
-
--- Disable Copilot for filetypes
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-	pattern = {
-		"*.md",
-	},
-	command = "Copilot disable",
-})
-
 -- Change shiftwidth to 2 for yaml, markdown
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 	pattern = {
