43de31bee4d83aa8eed0e68f2e30390e8f379190
Duc Dao <duc.dao.431@gmail.com>
2024-10-26 22:07:39 +0700

    update plugin list

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,16 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -128,90 +128,99 @@ require("lazy").setup({
 				require("inc_rename").setup()
 			end,
 		},
+		-- {
+		-- 	"epwalsh/obsidian.nvim",
+		-- 	version = "*", -- recommended, use latest release instead of latest commit
+		-- 	lazy = false,
+		-- 	ft = "markdown",
+		-- 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+		-- 	-- event = {
+		-- 	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+		-- 	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+		-- 	--   "BufReadPre path/to/my-vault/**.md",
+		-- 	--   "BufNewFile path/to/my-vault/**.md",
+		-- 	-- },
+		-- 	dependencies = {
+		-- 		-- Required.
+		-- 		"nvim-lua/plenary.nvim",
+		-- 	},
+		-- 	opts = {
+		-- 		workspaces = {
+		-- 			{
+		-- 				name = "personal",
+		-- 				path = "/home/itsdmd/Nextcloud/Documents/Obsidian/Personal",
+		-- 			},
+		-- 		},
+		-- 		completion = {
+		-- 			-- Set to false to disable completion.
+		-- 			nvim_cmp = true,
+		-- 			-- Trigger completion at 2 chars.
+		-- 			min_chars = 2,
+		-- 		},
+		--
+		-- 		mappings = {
+		-- 			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+		-- 			["gf"] = {
+		-- 				action = function()
+		-- 					return require("obsidian").util.gf_passthrough()
+		-- 				end,
+		-- 				opts = { noremap = false, expr = true, buffer = true },
+		-- 			},
+		-- 			-- Toggle check-boxes.
+		-- 			["<leader>ch"] = {
+		-- 				action = function()
+		-- 					return require("obsidian").util.toggle_checkbox()
+		-- 				end,
+		-- 				opts = { buffer = true },
+		-- 			},
+		-- 			-- Smart action depending on context, either follow link or toggle checkbox.
+		-- 			["<cr>"] = {
+		-- 				action = function()
+		-- 					return require("obsidian").util.smart_action()
+		-- 				end,
+		-- 				opts = { buffer = true, expr = true },
+		-- 			},
+		-- 		},
+		--
+		-- 		-- Optional, customize how note IDs are generated given an optional title.
+		-- 		---@param title string|?
+		-- 		---@return string
+		-- 		note_id_func = function(title)
+		-- 			-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+		-- 			-- In this case a note with the title 'My new note' will be given an ID that looks
+		-- 			-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+		-- 			local suffix = ""
+		-- 			if title ~= nil then
+		-- 				-- If title is given, transform it into valid file name.
+		-- 				suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+		-- 			else
+		-- 				-- If title is nil, just add 4 random uppercase letters to the suffix.
+		-- 				for _ = 1, 4 do
+		-- 					suffix = suffix .. string.char(math.random(65, 90))
+		-- 				end
+		-- 			end
+		-- 			return tostring(os.time()) .. "-" .. suffix
+		-- 		end,
+		--
+		-- 		-- Where to put new notes. Valid options are
+		-- 		--  * "current_dir" - put new notes in same directory as the current buffer.
+		-- 		--  * "notes_subdir" - put new notes in the default notes subdirectory.
+		-- 		new_notes_location = "current_dir",
+		--
+		-- 		-- Either "wiki" or "markdown".
+		-- 		preferred_link_style = "markdown",
+		-- 	},
+		-- },
 		{
-			"epwalsh/obsidian.nvim",
-			version = "*", -- recommended, use latest release instead of latest commit
-			lazy = false,
-			ft = "markdown",
-			-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-			-- event = {
-			--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-			--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-			--   "BufReadPre path/to/my-vault/**.md",
-			--   "BufNewFile path/to/my-vault/**.md",
-			-- },
+			"Exafunction/codeium.nvim",
 			dependencies = {
-				-- Required.
 				"nvim-lua/plenary.nvim",
+				"hrsh7th/nvim-cmp",
 			},
-			opts = {
-				workspaces = {
-					{
-						name = "personal",
-						path = "/home/itsdmd/Nextcloud/Documents/Obsidian/Personal",
-					},
-				},
-				completion = {
-					-- Set to false to disable completion.
-					nvim_cmp = true,
-					-- Trigger completion at 2 chars.
-					min_chars = 2,
-				},
-
-				mappings = {
-					-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-					["gf"] = {
-						action = function()
-							return require("obsidian").util.gf_passthrough()
-						end,
-						opts = { noremap = false, expr = true, buffer = true },
-					},
-					-- Toggle check-boxes.
-					["<leader>ch"] = {
-						action = function()
-							return require("obsidian").util.toggle_checkbox()
-						end,
-						opts = { buffer = true },
-					},
-					-- Smart action depending on context, either follow link or toggle checkbox.
-					["<cr>"] = {
-						action = function()
-							return require("obsidian").util.smart_action()
-						end,
-						opts = { buffer = true, expr = true },
-					},
-				},
-
-				-- Optional, customize how note IDs are generated given an optional title.
-				---@param title string|?
-				---@return string
-				note_id_func = function(title)
-					-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-					-- In this case a note with the title 'My new note' will be given an ID that looks
-					-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-					local suffix = ""
-					if title ~= nil then
-						-- If title is given, transform it into valid file name.
-						suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-					else
-						-- If title is nil, just add 4 random uppercase letters to the suffix.
-						for _ = 1, 4 do
-							suffix = suffix .. string.char(math.random(65, 90))
-						end
-					end
-					return tostring(os.time()) .. "-" .. suffix
-				end,
-
-				-- Where to put new notes. Valid options are
-				--  * "current_dir" - put new notes in same directory as the current buffer.
-				--  * "notes_subdir" - put new notes in the default notes subdirectory.
-				new_notes_location = "current_dir",
-
-				-- Either "wiki" or "markdown".
-				preferred_link_style = "markdown",
-			},
+			config = function()
+				require("codeium").setup({})
+			end,
 		},
-
 		{
 			import = "plugins",
 		},
